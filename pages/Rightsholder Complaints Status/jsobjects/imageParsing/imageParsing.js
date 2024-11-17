e1431a29835657298ce46369a41930ac7fef63ec
Jasmeet Singh <jasmeet.singh@bytescare.com>
2024-11-17 10:02:31 +0000

    done some fix

--- pages/Rightsholder Complaints Status/jsobjects/imageParsing/imageParsing.js
+++ pages/Rightsholder Complaints Status/jsobjects/imageParsing/imageParsing.js
@@ -2,15 +2,16 @@ export default {
 	imageHTML: "", 
 	async imageRendering() {
 		// Fetch image data from the query
-		let data = await getImageComplaintFormData.run();
+		let complaint_form_id = await handleTabChange.handleTabChange()[`${Tabs1.selectedTab}`].table2.triggeredRow.complaint_form_id;
+		let data = await getImageComplaintFormData.run({complaint_form_id:complaint_form_id});
 		let imageArray = data && data.length > 0 ? data.map(item => item.image_data) : [];
 
 		// Limit the number of images to display to 3
 
 		// Create HTML string for images with the correct prefix
-		this.imageHTML = imageArray.map(src => 
-																		`<img src="data:image/;base64,${src}" style="width: 100%; height: auto;" />
+		this.imageHTML = await imageArray.map(src => 
+																					`<img src="data:image/;base64,${src}" style="width: 100%; height: auto;" />
                         <br>`
-																	 ).join('');
+																				 ).join('');
 	}
 };
