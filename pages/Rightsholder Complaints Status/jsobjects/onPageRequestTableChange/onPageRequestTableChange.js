1790d5379ed88dfd20c2274b6126134060f25267
Jasmeet Singh <jasmeet.singh@bytescare.com>
2024-12-25 06:36:39 +0000

    done some fix test

--- pages/Rightsholder Complaints Status/jsobjects/onPageRequestTableChange/onPageRequestTableChange.js
+++ pages/Rightsholder Complaints Status/jsobjects/onPageRequestTableChange/onPageRequestTableChange.js
@@ -3,17 +3,33 @@ export default {
 		//	write code here
 		if (Tabs1.selectedTab === 'Pending Complaints') {
 			buildTableData.setQueryConditions('Pending Complaints', Table1, Table2);
+			await countTable1RequestData.run();
+			let data = await Table1ComplaintsUrlData.run();
+			console.log("data[0]",data[0]);
+			Object.assign(Table1.selectedRow, data[0]);
 		} else if (Tabs1.selectedTab === 'Approved Complaints') {
 			buildTableData.setQueryConditions('Approved Complaints', Table1Copy, Table2Copy);
+			await countTable1RequestData.run();
+			let data = await Table1ComplaintsUrlData.run();
+			Object.assign(Table1Copy.selectedRow, data[0]);
 		} else if (Tabs1.selectedTab === 'Rejected Complaints') {
 			buildTableData.setQueryConditions('Rejected Complaints', Table1Copy1, Table2Copy1);
+			await countTable1RequestData.run();
+			let data = await Table1ComplaintsUrlData.run();
+			Object.assign(Table1Copy1.selectedRow, data[0]);
+
 		} else if (Tabs1.selectedTab === 'Submitted For Blocking Complaints') {
 			buildTableData.setQueryConditions('Submitted For Blocking Complaints', Table1Copy2, Table2Copy2);
+			await countTable1RequestData.run();
+			let data = await Table1ComplaintsUrlData.run();
+			Object.assign(Table1Copy2.selectedRow, data[0]);
 		} else if (Tabs1.selectedTab === 'Blocked Complaints') {
 			buildTableData.setQueryConditions('Blocked Complaints', Table1Copy3, Table2Copy3);
+			await countTable1RequestData.run();
+			let data = await Table1ComplaintsUrlData.run();
+			Object.assign(Table1Copy3.selectedRow, data[0]);
 		}
-		await countTable1RequestData.run();
-		await Table1ComplaintsUrlData.run();
+
 		await buildTableData.tableData();
 	}
 }
